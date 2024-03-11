a2f7b7bd1f34bc18a254ba3881d16e15131ea736
aihaikuan <909093161@qq.com>
2024-03-11 23:42:33 +0800

    chore: 配置根目录，引入element

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+    },
   },
 })
